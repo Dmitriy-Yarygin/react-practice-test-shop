0b1f563c408d111d5149114b1017826c4d9c33e2
Dmitriy Yarygin <dmitriy.yarygin@yahoo.com>
2018-12-26 01:00:16 +0200

    suffering with tests

---
+++ src/Tests/ProductList.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ProductList from "../routes/ProductList/ProductList";
+import ProductCard from "../routes/ProductList/ProductCard";
+import { products } from "../db/yacht.json";
+
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+import { shallow, mount, render } from "enzyme";
+
+describe("<ProductList />", () => {
+  // it("allows us to set props", () => {
+  //   const wrapper = mount(
+  //     <ProductList bar="baz" items={[]} fetchProducts={() => {}} />
+  //   );
+  //   expect(wrapper.props().bar).to.equal("baz");
+
+  //   wrapper.setProps({ bar: "foo" });
+  //   expect(wrapper.props().bar).to.equal("foo");
+  // });
+
+  it("renders three <ProductCard /> components", () => {
+    const wrapper = shallow(
+      <ProductCard
+        item={products[0]}
+        fetchProducts={() => {}}
+        addProductToCart={() => {}}
+      />
+    );
+    console.log( products[0].name );
+
+    // <p class="MuiTypography-root-67 MuiTypography-body1-76">Current Price: 1,200</p>
+    console.log(wrapper);
+    // expect(wrapper.find(ProductCard)).to.have.lengthOf(3);
+  });
+});
--- src/routes/Product/Product.js
+++ src/routes/Product/Product.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import ProductNotFound from '../../common/ProductNotFound'
+import React, { Component } from "react";
+import ProductNotFound from "../../common/ProductNotFound";
 
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
-import CardMedia from '@material-ui/core/CardMedia'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
   root: {
@@ -15,23 +15,23 @@ const styles = theme => ({
   },
   paper: {
     padding: theme.spacing.unit * 2,
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary
   },
   media: {
     height: 0,
-    paddingTop: '56.25%' // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   button: {
     margin: theme.spacing.unit,
     width: 150
   }
-})
+});
 
 class Product extends Component {
-  componentWillMount () {
-    const id = Number(this.props.match.params.productId)
-    this.props.saveId(id)
+  componentWillMount() {
+    const id = Number(this.props.match.params.productId);
+    this.props.saveId(id);
     if (
       !(this.props.item && this.props.item.id && this.props.item.id === id) &&
       !(
@@ -39,22 +39,22 @@ class Product extends Component {
         this.props.items.filter(item => item.id === id).length > 0
       )
     ) {
-      this.props.getProductById(id)
+      this.props.getProductById(id);
     }
   }
 
   goBack = () => {
-    this.props.history.goBack()
-  }
+    this.props.history.goBack();
+  };
 
   buy = () => {
-    this.props.addProductToCart( this.props.item );
-  }
+    this.props.addProductToCart(this.props.item);
+  };
 
-  render () {
-    const id = Number(this.props.match.params.productId)
+  render() {
+    const id = Number(this.props.match.params.productId);
     if (!this.props.item || (this.props.item.id && this.props.item.id !== id)) {
-      return <ProductNotFound id={id} />
+      return <ProductNotFound id={id} />;
     }
     const {
       name,
@@ -65,42 +65,42 @@ class Product extends Component {
       material,
       description,
       cost
-    } = this.props.item
-    const { classes } = this.props
+    } = this.props.item;
+    const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <Typography variant='h2' component='h2' gutterBottom>
+        <Typography variant="h2" component="h2" gutterBottom>
           {name}
         </Typography>
         <Grid container spacing={24}>
           <Grid item xs={12} md={4}>
-            <Typography variant='body1' gutterBottom>
+            <Typography variant="body1" gutterBottom>
               <b>Year: </b> {year}
             </Typography>
-            <Typography variant='body1' gutterBottom>
+            <Typography variant="body1" gutterBottom>
               <b>Length: </b> {length}
             </Typography>
-            <Typography variant='body1' gutterBottom>
+            <Typography variant="body1" gutterBottom>
               <b>Located In: </b> {location}
             </Typography>
-            <Typography variant='body1' gutterBottom>
+            <Typography variant="body1" gutterBottom>
               <b>Hull Material: </b> {material}
             </Typography>
-            <Typography variant='body1' gutterBottom>
-              <b>Current Price: </b> {cost.toLocaleString() || cost}
+            <Typography variant="body1" gutterBottom>
+              <b>Current Price: </b> {cost.toLocaleString()}
             </Typography>
-            <Typography component='p'>{description}</Typography>
+            <Typography component="p">{description}</Typography>
             <Button
               onClick={this.goBack}
-              variant='outlined'
+              variant="outlined"
               className={classes.button}
             >
               Return
             </Button>
             <Button
               onClick={this.buy}
-              variant='outlined'
-              color='primary'
+              variant="outlined"
+              color="primary"
               className={classes.button}
             >
               Buy
@@ -110,17 +110,17 @@ class Product extends Component {
             <CardMedia
               className={classes.media}
               image={url}
-              title='Sail yacht'
+              title="Sail yacht"
             />
           </Grid>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
 Product.propTypes = {
   classes: PropTypes.object.isRequired
-}
+};
 
-export default withStyles(styles)(Product)
+export default withStyles(styles)(Product);
