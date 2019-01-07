115e7a3c700a0db619910fd901bb94075c1f60ec
Dmitriy Yarygin <dmitriy.yarygin@yahoo.com>
2019-01-07 17:49:30 +0200

    Add localStorage

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 import Layout from "./routes/Layout";
 import Router from "./routes/Router";
-
+import { loadState, saveState } from "./localStorage";
+import throttle from "lodash/throttle";
 import storeCreator from "./store";
 
-const store = storeCreator();
+const store = storeCreator(loadState());
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
 
 class App extends Component {
   render() {
